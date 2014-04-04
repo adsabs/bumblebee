ad49c3dd16fef4154aa7d64fbc26b442e45e6a5f
Alex <aholachek@gmail.com>
2014-04-04 15:57:50 -0400

    improved test, now has proper activate and render method

--- test/mocha/js/widgets/search_bar_widget.spec.js
+++ test/mocha/js/widgets/search_bar_widget.spec.js
@@ -1,29 +1,57 @@
-require(['js/widgets/search_bar/search_bar_view'], function(SearchBarView, SearchBarTemplate) {
-
-    describe("Search Bar (UI Widget)", function() {
-
-        it("should render a search bar and a submit button", function() {
-
-        	var a = new SearchBarView();
-        	a.render();
-            
-        	expect(a.$(".q").length).to.equal(1);
-        	expect(a.$(".search-submit").length).to.equal(1);
-        });
-
-        it("should submit the query when the search-submit button is pressed", function(){
+define(['jquery', 'js/widgets/search_bar/search_bar_widget', 'js/components/beehive', 'js/services/pubsub'], function($, SearchBarWidget, BeeHive, PubSub) {
 
 
+  describe("Search Bar (UI Widget)", function() {
 
-        });
+    var beehive, pubsub, key, widget, widget, $w;
 
-        it("should notify the system of any user interaction in the query input", function(){
+    before(function() {
+        
+      beehive = new BeeHive();
+      pubsub = new PubSub();
+      beehive.addService('PubSub', pubsub);
+      key = pubsub.getPubSubKey();
 
+      widget = new SearchBarWidget;
+      widget.activate(beehive.getHardenedInstance());
+      $w = $(widget.render());
 
-        })
+    $("#test").append($w);
 
+    });
 
-
+    after(function(){
+        $("#test").empty();
     })
+
+
+    it("should render a search bar and a submit button", function() {
+
+      expect($(".q").length).to.equal(1);
+      expect($(".search-submit").length).to.equal(1);
+    });
+
+    it("should trigger a NEW_QUERY when the search-submit button is pressed", function(done) {
+
+      var currentSearch;
+
+      pubsub.subscribe(key, pubsub.NEW_QUERY, function(apiQuery) {
+        currentSearch = apiQuery.get("q")[0]
+      })
+
+      $(".q").val("author:kurtz,m");
+      $(".search-submit").click();
+
+      expect(currentSearch).to.equal('author:kurtz,m');
+
+      done();
+
+    });
+
+    it("should allow the user to click to add fielded search words to search bar")
+
+
+
+  });
 
 })
