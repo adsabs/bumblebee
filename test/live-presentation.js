bd45230aab157ac03f62ea16d289277e671661a4
Alex <aholachek@gmail.com>
2014-05-28 10:08:11 -0400

    contents widget works with new version of marionette

--- test/live-presentation.js
+++ test/live-presentation.js
@@ -1,8 +1,14 @@
 require(['js/components/beehive', 'js/services/pubsub', 'js/components/query_mediator', 'js/services/api',
   'jquery', 'underscore',
   'js/widgets/search_bar/search_bar_widget', 'js/widgets/results/widget',
-  'js/widgets/facet/factory', 'js/widgets/query_info/query_info_widget'
-], function(BeeHive, PubSub, QueryMediator, Api, $, _, SearchBar, ResultsWidget, FacetFactory, QueryInfoWidget) {
+  'js/widgets/facet/factory', 'js/widgets/query_info/query_info_widget',
+  'js/widgets/abstract/widget', 'js/widgets/contents_manager/widget', 'js/widgets/references/widget'
+], function(BeeHive, PubSub, QueryMediator, Api, $, _, SearchBar, ResultsRender,
+  FacetFactory, QueryInfoWidget, AbstractWidget, LayoutWidget, ReferencesWidget) {
+
+  var abstract = new AbstractWidget();
+  var references = new ReferencesWidget();
+  var l = new LayoutWidget({widgetTitleMapping : {'abstract' : {widget: abstract, default : true}, 'references' : {widget : references}}})
 
 
   var beehive = new BeeHive();
@@ -11,131 +17,144 @@ require(['js/components/beehive', 'js/services/pubsub', 'js/components/query_med
   var queryMediator = new QueryMediator();
   queryMediator.activate(beehive);
 
-  var queryInfo = new QueryInfoWidget();
+  // var queryInfo = new QueryInfoWidget();
 
-  var searchBar = new SearchBar();
-  var results = new ResultsWidget({pagination: {rows: 40, start:0}});
-  var citationsGraphWidget = FacetFactory.makeGraphFacet({
-    facetField: "citation_count",
-    facetTitle: "Citations",
-    xAxisTitle: "Citation Count",
-    openByDefault: true
-  });
+  var s = new SearchBar();
+  // var r = new ResultsRender({pagination: {rows: 40, start:0}});
+  // var c = FacetFactory.makeGraphFacet({
+  //   facetName: "citation_count",
+  //   userFacingName: "Citations",
+  //   xAxisTitle: "Citation Count",
+  //   openByDefault: true,
+  // });
 
-  var authorFacets = FacetFactory.makeHierarchicalCheckboxFacet({
-    facetField: "author_facet_hier",
-    facetTitle: "Authors",
-    openByDefault: true,
-    logicOptions: {single: ['limit to', 'exclude'], 'multiple': ['and', 'or', 'exclude']},
-    responseProcessors: [
-      function(v) {var vv = v.split('/'); return vv[vv.length-1]}
-    ]
-  });
-  var keywords = FacetFactory.makeBasicCheckboxFacet({
-    facetField: "keyword_facet",
-    facetTitle: "Keywords",
-    openByDefault: false,
-    logicOptions: {single: ['limit to', 'exclude'], 'multiple': ['and', 'or', 'exclude']}
-  });
+  // var a = FacetFactory.makeHierarchicalCheckboxFacet({
+  //   facetName: "author",
+  //   userFacingName: "Authors",
+  //   openByDefault: true,
+  //   preprocess: ["titleCase", "removeSlash"]
+  // })
+  // var keywords = FacetFactory.makeBasicCheckboxFacet({
+  //   facetName: "keyword",
+  //   userFacingName: "Keywords",
+  //   openByDefault: false,
+  //   preprocess: ["titleCase", "removeSlash"]
+  // })
 
-  var database = FacetFactory.makeBasicCheckboxFacet({
-    facetField: "database",
-    facetTitle: "Collections",
-    openByDefault: true
-  });
-  var data = FacetFactory.makeBasicCheckboxFacet({
-    facetField: "data_facet",
-    facetTitle: "Data",
-    openByDefault: false
-  });
+  // var database = FacetFactory.makeBasicCheckboxFacet({
+  //   facetName: "database",
+  //   userFacingName: "Databases",
+  //   openByDefault: true,
+  //   preprocess: "titleCase",
+  // })
+  // var data = FacetFactory.makeBasicCheckboxFacet({
+  //   facetName: "data",
+  //   userFacingName: "Data",
+  //   openByDefault: false,
+  //   preprocess: "allCaps",
+  // })
 
-  var vizier = FacetFactory.makeBasicCheckboxFacet({
-    facetField: "vizier_facet",
-    facetTitle: "Vizier Tables",
-    openByDefault: false
-  });
+  // var vizier = FacetFactory.makeBasicCheckboxFacet({
+  //   facetName: "vizier",
+  //   userFacingName: "Vizier Tables",
+  //   openByDefault: false,
+  //   preprocess: "allCaps",
+  // })
 
-  var pub = FacetFactory.makeBasicCheckboxFacet({
-    facetField: "bibstem_facet",
-    facetTitle: "Publications",
-    openByDefault: false,
-    logicOptions: {single: null, multiple: ["or", "exclude"]}
-  });
-  var bibgroup = FacetFactory.makeBasicCheckboxFacet({
-    facetField: "bibgroup_facet",
-    facetTitle: "Bib Groups",
-    openByDefault: false,
-    logicOptions: {single: null, multiple: ["or", "exclude"]}
-  });
+  // var pub = FacetFactory.makeBasicCheckboxFacet({
+  //   facetName: "bibstem",
+  //   userFacingName: "Publications",
+  //   openByDefault: false,
+  //   preprocess: "allCaps",
+  //   multiLogic: ["or", "exclude"]
+  // })
+  // var bibgroup = FacetFactory.makeBasicCheckboxFacet({
+  //   facetName: "bibgroup",
+  //   userFacingName: "Bib Groups",
+  //   openByDefault: false,
+  //   preprocess: "allCaps",
+  //   multiLogic: ["or", "exclude"]
+  // })
 
-  var grants = FacetFactory.makeHierarchicalCheckboxFacet({
-    facetField: "grant_facet_hier",
-    facetTitle: "Grants",
-    openByDefault: false,
-    logicOptions: {single: null, multiple: ["or", "exclude"]}
-  });
+  // var grants = FacetFactory.makeHierarchicalCheckboxFacet({
+  //   facetName: "grant",
+  //   userFacingName: "Grants",
+  //   openByDefault: false,
+  //   preprocess: ["allCaps", "removeSlash"],
+  //   multiLogic: ["or", "exclude"]
+  // })
 
-  var refereed = FacetFactory.makeBasicCheckboxFacet({
-    facetField: "property",
-    facetTitle: "Refereed Status",
-    openByDefault: true,
-    extractFacets: function(facets) {
-      var returnList = [];
-      _.each(facets, function(d,i ){
-        if (d ==="refereed"){
-          returnList.push("Refereed", facets[i+1])
-        }
-        else if (d ==="notrefereed"){
-           returnList.push("Non-Refereed", facets[i+1])
-        }
-      })
-      return returnList
-    }
-  });
+  // var refereed = FacetFactory.makeBasicCheckboxFacet({
+  //   facetName: "property",
+  //   userFacingName: "Refereed Status",
+  //   openByDefault: true,
+  //   multiLogic: "fullSet",
+  //   extractFacets: function(facets) {
+  //     var returnList = [];
+  //     _.each(facets, function(d,i ){
+  //       if (d ==="refereed"){
+  //         returnList.push("Refereed", facets[i+1])
+  //       }
+  //       else if (d ==="notrefereed"){
+  //          returnList.push("Non-Refereed", facets[i+1])
+  //       }
+  //     })
+  //     return returnList
+  //   }
+  // })
 
-  var yearGraph = FacetFactory.makeGraphFacet({
-    facetField: "year",
-    facetTitle: "Year",
-    xAxisTitle: "Year",
-    openByDefault: true
-  });
+  // var y = FacetFactory.makeGraphFacet({
+  //   facetName: "year",
+  //   userFacingName: "Year",
+  //   xAxisTitle: "Year",
+  //   openByDefault: true,
+  // });
 
-  queryInfo.activate(beehive.getHardenedInstance());
-  searchBar.activate(beehive.getHardenedInstance());
-  results.activate(beehive.getHardenedInstance());
-  citationsGraphWidget.activate(beehive.getHardenedInstance());
-  authorFacets.activate(beehive.getHardenedInstance());
-  yearGraph.activate(beehive.getHardenedInstance());
-  database.activate(beehive.getHardenedInstance());
-  keywords.activate(beehive.getHardenedInstance());
-  pub.activate(beehive.getHardenedInstance());
-  bibgroup.activate(beehive.getHardenedInstance());
-  data.activate(beehive.getHardenedInstance());
-  vizier.activate(beehive.getHardenedInstance());
-  grants.activate(beehive.getHardenedInstance());
-  refereed.activate(beehive.getHardenedInstance());
+  abstract.activate(beehive.getHardenedInstance())
+  references.activate(beehive.getHardenedInstance())
 
-
-
-  $("#top").append(searchBar.render().el)
-
-  $("#middle").append(results.render().el)
-
-  $("#left").append(authorFacets.render().el);
-  $("#left").append(database.render().el);
-  $("#left").append(refereed.render().el);
-  $("#left").append(keywords.render().el);
-  $("#left").append(pub.render().el);
-  $("#left").append(bibgroup.render().el);
-  $("#left").append(data.render().el);
-  $("#left").append(vizier.render().el);
-  $("#left").append(grants.render().el);
+//  queryInfo.activate(beehive.getHardenedInstance())
+  s.activate(beehive.getHardenedInstance())
+//  r.activate(beehive.getHardenedInstance())
+//  c.activate(beehive.getHardenedInstance())
+//  a.activate(beehive.getHardenedInstance())
+//  y.activate(beehive.getHardenedInstance())
+//  database.activate(beehive.getHardenedInstance())
+//  keywords.activate(beehive.getHardenedInstance())
+//  pub.activate(beehive.getHardenedInstance())
+//  bibgroup.activate(beehive.getHardenedInstance())
+//  data.activate(beehive.getHardenedInstance())
+//  vizier.activate(beehive.getHardenedInstance())
+//  grants.activate(beehive.getHardenedInstance())
+//  refereed.activate(beehive.getHardenedInstance())
 
 
 
+  $("#top").append(s.getView().render().el)
 
-  $("#right").append(queryInfo.render().el);
-  //$("#right").append(yearGraph.render().el);
-  //$("#right").append(citationsGraphWidget.render().el);
+//  $("#middle").append(r.getView().render().el)
 
-});
+  $("#middle").append(l.render().el)
+
+//  $("#left").append(a.getView().render().el)
+//  $("#left").append(database.getView().render().el)
+//  $("#left").append(refereed.getView().render().el)
+//  $("#left").append(keywords.getView().render().el)
+//  $("#left").append(pub.getView().render().el)
+//  $("#left").append(bibgroup.getView().render().el)
+//  $("#left").append(data.getView().render().el)
+//  $("#left").append(vizier.getView().render().el)
+//  $("#left").append(grants.getView().render().el)
+
+
+
+
+  $("#right").append(queryInfo.getView().render().el)
+  $("#right").append(y.getView().render().el)
+  $("#right").append(c.getView().render().el)
+
+
+
+
+
+})
