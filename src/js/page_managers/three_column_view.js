2985b92b7d1a46ae770336c2e77d3c33046d1fc4
Alex <aholachek@gmail.com>
2014-11-12 16:25:57 -0500

    successfully added improved column expanding functionality and responsive third column

--- src/js/page_managers/three_column_view.js
+++ src/js/page_managers/three_column_view.js
@@ -6,21 +6,63 @@ define([
   ],
   function (Marionette,
             pageTemplate,
-            controlRowTemplate,
-            BaseWidget
+            controlRowTemplate
             ) {
+
+
+/*
+* keeps track of the open/closed state of the three columns
+* */
+    ResultsStateModel = Backbone.Model.extend({
+
+      defaults : function(){
+
+        return {
+          left : "open",
+          right : "open",
+          largerThanTablet : true
+
+        }
+      }
+    })
+
 
     var ThreeColumnView = Marionette.ItemView.extend({
 
       initialize : function(options){
         var options = options || {};
         this.widgets = options.widgets;
+        this.model = new ResultsStateModel;
+
+
+        var self = this;
+
+        $(window).resize(function() {
+
+          if (self.$(".right-expand").css("display") == "none") {
+            self.model.set("largerThanTablet", false)
+          }
+          else {
+            self.model.set("largerThanTablet", true)
+
+          }
+
+        });
+
       },
 
       template : pageTemplate,
 
-      events : {
-        "click .btn-expand" : "toggleColumns"
+
+      modelEvents: {
+        "change:left": "toggleColumns",
+        "change:right": "toggleColumns",
+        "change:largerThanTablet": "updateColumnContent"
+      },
+
+      events: {
+
+        "click .btn-expand": "toggleStateModel"
       },
 
       onRender : function(){
@@ -62,90 +104,177 @@ define([
         this.$(".s-left-col-container").toggle(show === null ? true : show);
       },
 
+      updateColumnContent: function () {
 
-      /**
-       * Show/hide - in a slide fashion - the columns when user clicks on the
-       * controls
-       *
-       * @param e
-       */
-      toggleColumns :function(e){
+        var leftHidden = (this.model.get("left") === "closed");
 
-        var $t = $(e.currentTarget);
-        var $leftCol =  this.$(".s-results-left-column");
-        var $rightCol =  this.$(".s-results-right-column");
+        if (this.model.get("largerThanTablet")) {
+          // it's a three column layout
 
-        if ($t.hasClass("btn-upside-down")){
+          this.$("#results-right-column").append(this.$(".right-col-container"))
 
-          $t.removeClass("btn-upside-down");
+          if (leftHidden) {
 
-          if ($t.hasClass("left-expand")){
-
-            $leftCol.removeClass("hidden-col");
-            $leftCol.find(".left-col-container").width('').fadeIn(500).children().show();
-
-          }
-          else {
-            $rightCol.removeClass("hidden-col");
-
-            $rightCol.find(".right-col-container").width('').fadeIn(500) ;
-
-          }
-
-          if (!$rightCol.hasClass("hidden-col") && !$leftCol.hasClass("hidden-col")){
-            this.$("#results-middle-column")
-              .css({"width": ""})
-
-          }
-          else if ($leftCol.hasClass("hidden-col")){
-            this.$("#results-middle-column")
-              .css({"width": "75%"})
-          }
-          else {
-            this.$("#results-middle-column")
-              .css({"width":  "83.33333333%"})
+            this.$(".right-col-container").show();
 
           }
 
         }
         else {
-          $t.addClass("btn-upside-down");
+          // two column layout
+          this.$("#results-left-column").append(this.$(".right-col-container"));
+          if (leftHidden) {
 
-          if ($t.hasClass("left-expand")){
-
-            $leftCol.find(".left-col-container").width(0).fadeOut(500).children().hide();
-
-            $leftCol.addClass("hidden-col")
-
-          }
-          else {
-            //expand to the right
-
-            $rightCol.find(".right-col-container").width(0).hide(500);
-
-            $rightCol.addClass("hidden-col");
-
-
-          }
-
-          if ($rightCol.hasClass("hidden-col") && $leftCol.hasClass("hidden-col")){
-            this.$("#results-middle-column")
-              .addC //XXX: what is this???
-
-          }
-          else if ($rightCol.hasClass("hidden-col")){
-            this.$("#results-middle-column")
-              // 58.33333 + 25
-              .css("width", "83.33333333%")
-          }
-          else {
-            //58.33333 + 16.666666
-            this.$("#results-middle-column")
-              .css("width", "75%")
-
+            this.$(".right-col-container").show();
           }
 
         }
+      },
+
+        /**
+       * Show/hide - in a slide fashion - the columns when user clicks on the
+       * controls
+       *
+       * @param e
+       */
+      toggleStateModel: function (e) {
+
+        var name, $button, state;
+
+        $button = $(e.currentTarget);
+
+        $button.toggleClass("btn-reversed");
+
+        name = $button.hasClass("left-expand") ? "left" : "right";
+
+        state = this.model.get(name) === "open" ? "closed" : "open";
+
+        this.model.set(name, state);
+
+      },
+
+      returnBootstrapClasses: function () {
+
+        var classes = this.classList;
+        var toRemove = []
+        _.each(classes, function (c) {
+          if (c.indexOf("col-") !== -1) {
+            toRemove.push(c)
+          }
+        })
+        return toRemove.join(" ")
+      },
+
+      makeCenterFullWidth: function () {
+
+        this.model.set("left", "closed");
+
+        this.model.set("right", "closed");
+
+      },
+
+      returnColWidthsToDefault: function () {
+
+        this.model.set("left", "open");
+
+        this.model.set("right", "open");
+
+      },
+
+
+      toggleColumns: function (e) {
+
+        var leftState, rightState, $leftCol, $rightCol, $middleCol;
+
+        leftState = this.model.get("left");
+
+        rightState = this.model.get("right");
+
+        //this will remove all bootstrap column classes, it's used below
+
+        $leftCol = this.$("#results-left-column");
+        $rightCol = this.$("#results-right-column");
+        $middleCol = this.$("#results-middle-column");
+
+        if (leftState === "open" && rightState === "open") {
+
+          $leftCol.removeClass("hidden-col")
+
+            setTimeout(function(){
+
+              $leftCol.children().show(0)
+
+            }, 500)
+
+
+          $rightCol.removeClass("hidden-col")
+
+          setTimeout(function(){
+
+            $rightCol.find(".right-col-container").show(0);
+
+          }, 500)
+
+
+          $middleCol.removeClass(this.returnBootstrapClasses)
+            .addClass("col-md-7 col-sm-8")
+
+
+        }
+        else if (leftState === "closed" && rightState === "open") {
+
+          $rightCol.removeClass("hidden-col")
+            .find(".right-col-container")
+
+          setTimeout(function(){
+
+            $rightCol.find(".right-col-container").show(0);
+
+          }, 500)
+
+          $leftCol
+            .addClass("hidden-col")
+            .children().hide();
+
+          $middleCol.removeClass(this.returnBootstrapClasses)
+            .addClass("col-md-9 col-sm-12")
+
+        }
+
+        else if (leftState === "open" && rightState === "closed") {
+
+          $leftCol.removeClass("hidden-col")
+
+          setTimeout(function(){
+
+            $leftCol.children().show(0)
+
+          }, 500)
+
+          $rightCol.addClass("hidden-col")
+            .find(".right-col-container").hide()
+
+          $middleCol.removeClass(this.returnBootstrapClasses)
+            .addClass("col-md-10 col-sm-8")
+
+        }
+
+        else if (leftState === "closed" && rightState === "closed") {
+
+          $rightCol.addClass("hidden-col")
+            .find(".right-col-container")
+            .hide();
+
+          $leftCol.addClass("hidden-col")
+            .children()
+            .hide();
+
+          $middleCol.removeClass(this.returnBootstrapClasses)
+            .addClass("col-md-12 col-sm-12")
+
+        }
+
+
       }
 
     });
