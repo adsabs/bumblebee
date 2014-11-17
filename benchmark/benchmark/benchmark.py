64b593892dfd888f784144d7eee7e3d2b4f81811
Jonny Elliott, Work Machine <jonnynelliott@googlemail.com>
2014-11-17 17:48:24 +0100

    Selenium tests working on VM setup

--- benchmark/benchmark/benchmark.py
+++ benchmark/benchmark/benchmark.py
@@ -5,14 +5,15 @@ import sys
 from logging.handlers import RotatingFileHandler
 from selenium.webdriver import Firefox, FirefoxProfile
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from xvfbwrapper import Xvfb
 
 STRESS_PATH = "/vagrant/benchmark"
-FIREBUG_EXTENSION = "{0}/firebug-2.0.6.xpi".format(STRESS_PATH)
-NETEXPORT_EXTENSION = "{0}/netExport-0.9b6.xpi".format(STRESS_PATH)
+FIREBUG_EXTENSION = "{0}/plugins/firebug-2.0.6.xpi".format(STRESS_PATH)
+NETEXPORT_EXTENSION = "{0}/plugins/netExport-0.9b6.xpi".format(STRESS_PATH)
 
 __author__ = "Jonny Elliott"
 __version__ = "0.1"
@@ -61,6 +62,27 @@ class WebPage(Firefox):
     self.logout_time = -99
 
     Firefox.__init__(self, firefox_profile=self.profile)
+
+
+  def log_fail(self, failure):
+
+    self.logger.warning("Failure: {0}".format(failure))
+    name_stamp = time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime())
+    self.logger.warning("Dumping contents to file: {0}".format(name_stamp))
+
+    self.take_screenshot("FAIL_DUMP_{0}.png".format(name_stamp))
+    self.dump_html("FAIL_DUMP_{0}.html".format(name_stamp))
+
+  def take_screenshot(self, screenshot_out):
+
+    self.logger.info("Taking screenshot")
+    self.save_screenshot("{0}/dump/{1}".format(STRESS_PATH, screenshot_out))
+
+  def dump_html(self, html_out):
+
+    self.logger.info("Writing HTML to file")
+    with open("{0}/dump/{1}".format(STRESS_PATH, html_out), 'w') as f:
+            f.write(self.page_source.encode('utf-8'))
 
   def check_config(self, config):
     """
@@ -150,6 +172,32 @@ class WebPage(Firefox):
     
     return config["headless"]
 
+  def waitFor(self, wait):
+    
+    try:
+      wait_type = getattr(By, wait["type"])
+      wait_value = wait["value"]
+    except ValueError:
+      self.logger.warning("You gave the format")
+      self.quit()
+      sys.exit()
+
+    try:
+      element = WebDriverWait(self, self.minimum_timeout).until(EC.presence_of_element_located((wait_type, wait_value)))
+
+    except TimeoutException:
+      self.log_fail(TimeoutException)
+      self.timed_out = True
+
+      element = Null
+
+    except:
+      self.logger.error("Unexpected behaviour. Terminating.")
+      self.quit()
+      sys.exit()
+
+    return element
+
   def pageLoad(self, wait=False):
 
     self.logger.info("Loading page.")
@@ -158,16 +206,7 @@ class WebPage(Firefox):
     self.get(self.url)
 
     if wait:
-      try:
-        element = WebDriverWait(self, self.minimum_timeout).until(EC.presence_of_element_located((By.ID, wait)))
-
-      except TimeoutException:
-        self.logger.warning("TimeoutException thrown. Continuing.")
-        self.timed_out = True
-
-      except:
-        self.logger.error("Unexpected behaviour. Terminating.")
-        sys.exit()
+      success = self.waitFor(wait)
 
     end_time = time.time()
 
@@ -176,10 +215,21 @@ class WebPage(Firefox):
     self.logger.info("Page loaded successfully.")
 
 
-  def sendQuery(self, query="Elliott"):
+  def sendQuery(self, query="Elliott", wait=False):
 
-    input_query = self.find_element(value="//input[@class=\"form-control q\"]", by=By.XPATH)
-    input_query.send_keys(query).submit()
+    input_query = self.find_element(value="//input[starts-with(@class,\"form-control q\")]", by=By.XPATH)
+    input_query.send_keys(query)
+
+    input_query.send_keys(Keys.RETURN) # click() and submit() do not work
+
+    if wait:
+      element = self.waitFor(wait)
+    else:
+      element = Null
+
+    self.logger.info("Search completed")
+
+    return element
 
 # Obsolete code
 
--- benchmark/test/example_unit_test.py
+++ benchmark/test/example_unit_test.py
@@ -3,6 +3,7 @@ import time
 from benchmark import WebPage
 
 localhost = "http://localhost:8000"
+#localhost = "http://adslabs.org/bumblebee/"
 
 class Test(unittest.TestCase):
 
@@ -14,6 +15,8 @@ class Test(unittest.TestCase):
     try:
       self.assertEqual("Bumblebee discovery", myPage.title)
       self.assertTrue(myPage.load_time > 0)
+    except Exception:
+      myPage.log_fail(Exception)
     finally:
       myPage.quit()
 
@@ -30,46 +33,29 @@ class Test(unittest.TestCase):
     myPage = WebPage(localhost, config=config)
     try:
       self.assertTrue("fake_input" not in myPage.config)
+    except Exception:
+      myPage.log_fail(Exception)
     finally:
       myPage.quit()
 
   def test_query_author(self):
-    myPage = WebPage(localhost)
+
+    success_text = ""
+
+    config = {"headless": True, "fire_bug": True}
+
+    myPage = WebPage(localhost, config=config)
+    myPage.pageLoad(wait={"type": "CLASS_NAME", "value": "q"})
+    #time.sleep(30)
+    class_identify = "col-sm-4 col-xs-5 s-top-row-col identifier s-identifier"
+    element = myPage.sendQuery(query="Elliott", wait={"type": "CLASS_NAME", "value": class_identify})
+
+    success_text = myPage.find_element_by_class_name(class_identify)
+
     try:
-      myPage.pageLoad()
-      myPage.sendQuery(query="Elliott")
-      success_text = myPage.find_element_by_class_name("s-results-control-row-container")
-      print success_text
-#    self.assertTrue("Einmal anmelden." in success_text)
+      self.assertTrue("Elliott" in success_text.text)
+    except Exception:
+      myPage.log_fail(Exception)
     finally:
       myPage.quit()
-#    time.sleep(5)
-
-#  def test_timing_logout_page(self):
-#    myPage = WebPage("http://google.de")
-#    myPage.pageLoad()
-#    myPage.pageLogout(logout_value="gbqfba", confirm_value="//button[@id=\"gbqfbb\"]", wait="logo")
-#    myPage.quit()
-#    time.sleep(5)
-
-#  def test_firebug_output(self):
-#    import glob
-#
-#    config = {"fire_bug": True, "headless": False, "net_export": True}
-#    myPage = WebPage("http://www.google.de", config=config)
-#
-#    num_files_start = glob.glob("{0}/*.har".format(myPage.har_output))
-#
-#    time.sleep(5)
-#    myPage.pageLoad()
-#    time.sleep(5)
-#
-#    myPage.quit()
-#
-#    num_files_end = glob.glob("{0}/*.har".format(myPage.har_output))
-#
-#    self.assertTrue(num_files_end > num_files_start)
-#
-#    time.sleep(5)
-
 
