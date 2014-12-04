264c752d9da66c140c48b9d1f235301fd25573bb
Alex <aholachek@gmail.com>
2014-12-04 14:24:04 -0500

    changed test to make it pass phantomjs

--- test/mocha/js/widgets/paper_network_widget.spec.js
+++ test/mocha/js/widgets/paper_network_widget.spec.js
@@ -749,6 +749,26 @@ define([
 
     });
 
+    it("should have a help popover", function(){
+
+      var paperNetwork = new PaperNetwork();
+
+      paperNetwork.processResponse(new JsonResponse(testDataBig));
+
+      $("#test").append(paperNetwork.view.el);
+
+      $("#test").find("i.icon-help").mouseover();
+
+    expect($(".popover").text()).to.eql("About This Network VisualizationThe paper network groups papers from your search results based on how many references they have in common. Papers with many references in common are more likely to discuss similar topics.If your search results returned a large enough set of papers, you will see two views: a summary view, which shows groups of tightly linked papers, and a detail view  which shows you the individual papers from a group and how they are connected. The size of the circles in the summary node graph are based on the cumulative number of citations shared by the group, and the titles of the summary nodes are small word clouds based on the words from the titles of the papers in the group.");
+
+     expect($(".popover").css("display")).to.eql("block");
+
+     $("#test").find("i.icon-help").mouseout();
+
+
+      debugger;
+    })
+
 
     it("should render small word clouds for the summary nodes", function(){
 
@@ -787,7 +807,7 @@ define([
 
     });
 
-    it.skip("should add mouseover interactions for the detail graph", function(){
+    it("should add mouseover interactions for the detail graph", function(){
 
       var paperNetwork = new PaperNetwork();
 
@@ -795,18 +815,25 @@ define([
 
       $("#test").append(paperNetwork.view.el);
 
-      //trigger detail view
-      $(".summary-node-group:first-of-type").click();
+//      //trigger detail view sadly can't trigger mouse events on svg in phantomjs
+//      $(".summary-node-group:first-of-type").click();
+//
+//      $(".detail-node:first-of-type").mouseover();
+//
+//      expect($(".popover").length).to.eql(1);
+//
+//      expect($(".popover").text()).to.eql("1991ASPC...13...73MTitle: The role of dense cores in isolated and cluster star formation.First Author: Myers, P. C.Citation Count: 6");
+//
+//      expect($(".popover").css("display")).to.eql("block");
+//
+//      $(".detail-node:first-of-type").mouseout();
 
-      $(".detail-node:first-of-type").mouseover();
+      paperNetwork.view.graphView.model.set("currentGroup", $(".summary-node-group:first-of-type").get(0));
 
-      expect($(".popover").length).to.eql(1);
+      expect( $(".detail-node:first-of-type").data("content")).to.eql("<b>Title: </b>The role of dense cores in isolated and cluster star formation.<br/><b>First Author: </b>Myers, P. C.<br/><b>Citation Count: </b>6");
 
-      expect($(".popover").text()).to.eql("1991ASPC...13...73MTitle: The role of dense cores in isolated and cluster star formation.First Author: Myers, P. C.Citation Count: 6");
+      expect( $(".detail-node:last-of-type").data("content")).to.eql("<b>Title: </b>Steepest descent technique and stellar equilibrium statistical mechanics. IV. Gravitating systems with an energy cutoff.<br/><b>First Author: </b>Katz, J.<br/><b>Citation Count: </b>21");
 
-      expect($(".popover").css("display")).to.eql("block");
-
-      $(".detail-node:first-of-type").mouseout();
 
 
     })
