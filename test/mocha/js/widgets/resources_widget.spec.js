2cc1d24adea86fcf71ee6088a929bff36923e866
Tim Hostetler <timothy.hostetler@cfa.harvard.edu>
2017-07-19 12:00:57 -0400

    Remove Loading Component Tests

--- test/mocha/js/widgets/resources_widget.spec.js
+++ test/mocha/js/widgets/resources_widget.spec.js
@@ -6,12 +6,11 @@ define([
     'es6!js/widgets/resources/widget.jsx',
     'es6!js/widgets/resources/components/fullTextSources.jsx',
     'es6!js/widgets/resources/components/dataProducts.jsx',
-    'es6!js/widgets/resources/components/loading.jsx',
     'js/widgets/resources/actions',
     'js/widgets/base/base_widget',
     'js/bugutils/minimal_pubsub'
   ],
-  function ($, React, Enzyme, ResourcesWidget, FullTextSources, DataProducts, LoadingIcon, actions, BaseWidget, MinPubSub) {
+  function ($, React, Enzyme, ResourcesWidget, FullTextSources, DataProducts, actions, BaseWidget, MinPubSub) {
 
   var mockResponse = {
     get: function () {
@@ -393,24 +392,6 @@ define([
         var source = mockLinksParsedData.dataProducts[idx];
         expect(arg[0]).to.equal(source.title);
       });
-    });
-
-    it('renders Loading component correctly', function () {
-      widget.activate(beehive);
-
-      var wrapNoShow = Enzyme.shallow(React.createElement(LoadingIcon, {
-        show: false 
-      }));
-      var wrapWithShow = Enzyme.shallow(React.createElement(LoadingIcon, { 
-        show: true 
-      }));
-      
-      // No output
-      expect(wrapNoShow.find('span').exists()).to.be.false;
-
-      // should return the proper output
-      expect(wrapWithShow.find('span').exists()).to.be.true;
-      expect(wrapWithShow.find('i').exists()).to.be.true;
     });
   });
 });
