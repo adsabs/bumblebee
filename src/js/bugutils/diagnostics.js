5e5b49a608d98cf5ba78c0823214f257c33b2f4c
Tim Hostetler <timothy.hostetler@cfa.harvard.edu>
2018-07-02 12:20:55 -0400

    auto-fix ./src/js/bugutils

--- src/js/bugutils/diagnostics.js
+++ src/js/bugutils/diagnostics.js
@@ -17,7 +17,7 @@ define([
   'js/components/generic_module',
   'sprintf',
   'js/components/api_targets'
-], function(
+], function (
   $,
   _,
   ApiQuery,
@@ -27,18 +27,17 @@ define([
   GenericModule,
   sprintf,
   ApiTargets
-  ) {
-
+) {
   var Diagnostics = GenericModule.extend({
 
-    activate: function(beehive, app) {
+    activate: function (beehive, app) {
       this.setApp(app);
       this.setBeeHive(beehive);
     },
 
-    getFirstDoc: function(queryString, options) {
+    getFirstDoc: function (queryString, options) {
       var opts = {
-        query: {'q': queryString, fl: 'title,abstract,bibcode,id,author'},
+        query: { q: queryString, fl: 'title,abstract,bibcode,id,author' },
         target: ApiTargets.SEARCH
       };
       if (options) {
@@ -46,8 +45,8 @@ define([
       }
       var promise = $.Deferred();
       this.apiRequest(opts)
-        .done(function(res) {
-          promise.resolve(res['response']['docs'][0]);
+        .done(function (res) {
+          promise.resolve(res.response.docs[0]);
         });
       return promise.promise();
     },
@@ -62,16 +61,14 @@ define([
      *    }
      * @returns Deferred
      */
-    apiRequest: function(options, reqOptions) {
+    apiRequest: function (options, reqOptions) {
       var app = this.getApp();
       var api = app.getService('Api');
       options = options || {};
-      if (!options.query)
-        throw Error('You must pass in "query"');
-      if (!(options.query instanceof ApiQuery))
-        options.query = new ApiQuery(options.query);
+      if (!options.query) throw Error('You must pass in "query"');
+      if (!(options.query instanceof ApiQuery)) options.query = new ApiQuery(options.query);
 
-      var r = new ApiRequest({'target': options.target || ApiTargets.SEARCH, query: options.query});
+      var r = new ApiRequest({ target: options.target || ApiTargets.SEARCH, query: options.query });
       return api.request(r, reqOptions);
     },
 
@@ -83,7 +80,7 @@ define([
      * @param options
      * @returns {*}
      */
-    ajax: function(options) {
+    ajax: function (options) {
       var app = this.getApp();
       var api = app.getService('Api');
       options.headers = options.header || {};
@@ -97,23 +94,23 @@ define([
      *
      * @param options
      */
-    jsonp: function(options, jsonpParameter) {
+    jsonp: function (options, jsonpParameter) {
       options.dataType = 'jsonp';
-      options.beforeSend = function(promise, xhr) {
+      options.beforeSend = function (promise, xhr) {
         var parts = xhr.url.split('&');
-        _.each(parts, function(p) {
+        _.each(parts, function (p) {
           if (p.startsWith('callback=')) {
             xhr.url += '&' + (jsonpParameter || 'json.wrf') + '=' + p.replace('callback=', ''); // jQuery generates a random callback, we'll pass it to solr
           }
-        })
+        });
       };
       return this.ajax(options);
     },
 
-    printComparison: function(result) {
+    printComparison: function (result) {
       var res = [];
 
-      var format= '%40s %40s %40s %10s';
+      var format = '%40s %40s %40s %10s';
       res.push(sprintf.sprintf(format, 'field', result.first, result.second, 'diff'));
       res.push('------------------------------------------------------------------------------------------------------------------------------------');
 
@@ -125,27 +122,24 @@ define([
       return res;
     },
 
-    compareTwoSearchInstances: function(url1, url2, listOfFields, listOfFields2) {
+    compareTwoSearchInstances: function (url1, url2, listOfFields, listOfFields2) {
       var self = this;
       var result = $.Deferred();
 
       if (!listOfFields || !listOfFields2) {
         var d1 = this.getListOfFields(url1);
         var d2 = this.getListOfFields(url2);
-        $.when(d1, d2).done(function(v1, v2) {
-
+        $.when(d1, d2).done(function (v1, v2) {
           self.compareTwoSearchInstances(url1, url2, v1, v2)
-            .done(function(res) {
+            .done(function (res) {
               result.resolve(res);
-            })
+            });
         });
-      }
-      else {
-
+      } else {
         var c1 = this.countDocsInFields(url1, listOfFields);
         var c2 = this.countDocsInFields(url2, listOfFields2);
         $.when(c1, c2)
-          .done(function(v1, v2) {
+          .done(function (v1, v2) {
             var fV1 = _.keys(v1.fields);
             var fV2 = _.keys(v2.fields);
             var fAll = _.unique(fV1.concat(fV2)).sort();
@@ -159,30 +153,29 @@ define([
                 first: o ? o.numFound : '--',
                 second: n ? n.numFound : '--',
                 diff: ((o && n) ? n.numFound - o.numFound : '--')
-              }
+              };
             }
-            result.resolve({first: url1, second: url2, fields: res});
+            result.resolve({ first: url1, second: url2, fields: res });
           });
       }
 
       return result;
     },
 
-    getListOfFields: function(url) {
+    getListOfFields: function (url) {
       var defer = $.Deferred();
       this.jsonp({
         url: url + '/solr/collection1/admin/luke?numTerms=0&wt=json&indent=true',
         timeout: 60000
       })
-      .done(function(data) {
-        var fields = [];
-        for(var fname in data.fields) {
-          if (fname.startsWith('_'))
-            continue;
-          fields.push(fname);
-        }
-        defer.resolve(fields);
-      });
+        .done(function (data) {
+          var fields = [];
+          for (var fname in data.fields) {
+            if (fname.startsWith('_')) continue;
+            fields.push(fname);
+          }
+          defer.resolve(fields);
+        });
       return defer;
     },
 
@@ -198,8 +191,7 @@ define([
    *     }
    *   }
      */
-    countDocsInFields: function(url, listOfFields){
-
+    countDocsInFields: function (url, listOfFields) {
       var result = $.Deferred();
       var self = this;
 
@@ -210,7 +202,7 @@ define([
         fields: collectedData
       };
 
-      for(var ix in listOfFields) {
+      for (var ix in listOfFields) {
         var fname = listOfFields[ix];
         var de = $.Deferred();
         var q = '{!lucene}' + fname + ':*';
@@ -219,36 +211,35 @@ define([
 
         var c = self.jsonp({
           url: url + '/solr/collection1/select?q=' + q + '&fl=id&wt=json&indent=true',
-          context: {field: fname, finalResult:finalResult, cycleR: cycleR},
+          context: { field: fname, finalResult: finalResult, cycleR: cycleR },
           timeout: 300000 // 5mins
         })
-          .done(function(data) {
-            this.finalResult.fields[this.field] = {numFound: data.response.numFound};
+          .done(function (data) {
+            this.finalResult.fields[this.field] = { numFound: data.response.numFound };
             var c = 0;
             var togo = [];
             for (var x in this.cycleR) {
-              if (this.cycleR[x].state() == 'pending') { //resolved and rejected count as done
+              if (this.cycleR[x].state() == 'pending') { // resolved and rejected count as done
                 c += 1;
                 togo.push(x);
               }
             }
             console.log(url + ' Got response for: ' + this.field + ' numFound: ' + data.response.numFound + ' requests to go: ' + c + ' ' + (togo.length < 5 ? togo.join(', ') : ''));
-            if (c == 0)
-              result.resolve(this.finalResult);
+            if (c == 0) result.resolve(this.finalResult);
           });
         cycleR[fname] = c;
         cycle.push(c);
       }
 
       // for some stupid reason this is not working, and i dont know why
-      //var d = $.when.call($.when, cycle)
+      // var d = $.when.call($.when, cycle)
       //  .done(function(data) {
       //    result.resolve(collectedData);
       //  });
       return result;
     },
 
-    request: function(options) {
+    request: function (options) {
       return $.ajax(options);
     },
 
@@ -261,7 +252,7 @@ define([
      */
 
 
-    testOrcidLogin: function() {
+    testOrcidLogin: function () {
       var oa = this.getApp().getService('OrcidApi');
       oa.signOut();
       window.location = oa.config.loginUrl;
@@ -271,43 +262,39 @@ define([
      * Upload one document to orcid;
      * Prerequisite: testOrcidLogin()
      */
-    testOrcidSendingData: function() {
+    testOrcidSendingData: function () {
       var app = this.getApp();
       var self = this;
       this.getFirstDoc('bibcode:1978yCat.1072....0C').done(
-        function(doc) {
+        function (doc) {
           var oa = app.getService('OrcidApi');
           oa.addWorks([doc])
-            .done(function(result) {
+            .done(function (result) {
               console.log('result:', result, 'expected: {}');
-            })
+            });
         }
-      )
+      );
     },
 
 
-    printPubSubSubscribers: function(printAllKeys) {
+    printPubSubSubscribers: function (printAllKeys) {
       var pubsub = this.getApp().getService('PubSub');
       var app = this.getApp();
 
 
       // collect names of all the objects
       var objNames = {};
-      app.triggerMethodOnAll(function(name, opts) {
-
+      app.triggerMethodOnAll(function (name, opts) {
         if (this === pubsub) { // pubsub has its own (root) key
           objNames[pubsub.pubSubKey.getId()] = name;
-        }
-        else if(this.hasPubSub && this.hasPubSub()) {
+        } else if (this.hasPubSub && this.hasPubSub()) {
           var ps = this.getPubSub();
           if (ps.getCurrentPubSubKey) {
-            if (objNames[ps.getCurrentPubSubKey().getId()])
-              console.warn('Redefining key that already exists: ' + ps.getCurrentPubSubKey().getId() + ' ' + objNames[ps.getCurrentPubSubKey().getId()])
+            if (objNames[ps.getCurrentPubSubKey().getId()]) console.warn('Redefining key that already exists: ' + ps.getCurrentPubSubKey().getId() + ' ' + objNames[ps.getCurrentPubSubKey().getId()]);
 
             objNames[ps.getCurrentPubSubKey().getId()] = name;
           }
-        }
-        else {
+        } else {
           console.warn('Instance without active PubSub: ' + name, this);
         }
       });
@@ -317,30 +304,26 @@ define([
 
         _.each(_.keys(pubsub._issuedKeys), function (k) {
           var p;
-          if (pubsub._issuedKeys[k].getId && objNames[pubsub._issuedKeys[k].getId()])
-            p = objNames[pubsub._issuedKeys[k].getId()];
+          if (pubsub._issuedKeys[k].getId && objNames[pubsub._issuedKeys[k].getId()]) p = objNames[pubsub._issuedKeys[k].getId()];
 
-          if (p)
-            console.log(k + '(parent:' + p + ') -> ' + objNames[k]);
-          else
-            console.log(k + ' -> ' + objNames[k]);
+          if (p) console.log(k + '(parent:' + p + ') -> ' + objNames[k]);
+          else console.log(k + ' -> ' + objNames[k]);
         });
       }
 
       var stats = {};
       console.log('Printing active subscribers to PubSub');
-      _.each(pubsub._events, function(val, key, idx) {
+      _.each(pubsub._events, function (val, key, idx) {
         console.log(key + ' (' + val.length + ')');
-        _.each(val, function(v) {
+        _.each(val, function (v) {
           var x = v.ctx.getId ? objNames[v.ctx.getId()] : 'unknown object';
           console.log('\t' + x);
           stats[x] = stats[x] ? stats[x] + 1 : 1;
           if (x == undefined) {
-            if (!stats['undefined keys'])
-              stats['undefined keys'] = [];
+            if (!stats['undefined keys']) stats['undefined keys'] = [];
             stats['undefined keys'].push(key + (v.ctx.getId ? v.ctx.getId() : 'X'));
           }
-        })
+        });
       });
 
       console.log(JSON.stringify(stats, null, ' '));
