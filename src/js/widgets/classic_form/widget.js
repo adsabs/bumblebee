919408e566d13c96e8a46902c9a716249832d860
Tim Hostetler <timothy.hostetler@cfa.harvard.edu>
2019-04-17 16:22:41 -0400

    remove date dropdowns on classic form

--- src/js/widgets/classic_form/widget.js
+++ src/js/widgets/classic_form/widget.js
@@ -338,47 +338,6 @@ define([
           });
         return $li;
       };
-
-      var resizeMenu = function () {
-        this.menu.element.outerHeight(300);
-        this.menu.element.attr('style', this.menu.element.attr('style') + ' overflow-x: hidden; position: relative;');
-      };
-
-      var onSelect = function (e, ui) {
-        var data = {};
-        data[$(this).attr('name')] = ui.item.value;
-        var val = _.extend({}, self.model.get('pubdate'), data);
-        self.model.set('pubdate', val);
-        this.value = ui.item.value;
-        e.preventDefault();
-        return false;
-      };
-
-      this.$('input[name="month_from"], input[name="month_to"]').autocomplete({
-        source: MONTHS,
-        minLength: 0,
-        select: onSelect
-      })
-      .click(showOnFocus)
-      .focus(showOnFocus);
-
-      this.$('input[name="year_from"]').autocomplete({
-        source: YEARS,
-        minLength: 0,
-        select: onSelect
-      })
-      .click(showOnFocus)
-      .focus(showOnFocus)
-      .data('ui-autocomplete')._resizeMenu = resizeMenu;
-
-      this.$('input[name="year_to"]').autocomplete({
-        source: YEARS,
-        minLength: 0,
-        select: onSelect
-      })
-      .click(showOnFocus)
-      .focus(showOnFocus)
-      .data('ui-autocomplete')._resizeMenu = resizeMenu;
     }
   });
 
