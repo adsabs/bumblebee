5b3d5dc9a1ebbbc6d7712f9922a0a1c3b3e874ac
Tim Hostetler <thostetler@users.noreply.github.com>
2019-08-13 22:34:11 -0400

    fixes to shim for ads core

--- src/shim.js
+++ src/shim.js
@@ -1,5 +1,6 @@
 'use strict';
 (function () {
+
   /*
     Dynamically pick which configuration to use based on the url.
     Then attempt to load the resource, using require, upon failure we
@@ -12,8 +13,8 @@
   };
 
   var load;
+  var version = window.APP_VERSION ? '?v=' + window.APP_VERSION : '';
   try {
-    var version = APP_VERSION ? '.' + APP_VERSION : '';
     var loc = window.location;
     var parts = loc[loc.pathname === '/' ? 'hash' : 'pathname'].replace(/#/g, '').split('/');
     var path = parts.reverse().filter(function (p) {
@@ -21,21 +22,21 @@
     });
     path = path.length && path[0];
     load = function () {
-        // attempt to get bundle config
-        require([paths[path] + '.config' + version], function () {}, function() {
+      // attempt to get bundle config
+      require([paths[path] + '.config.js' + version], function () {}, function () {
         // on failure to load specific bundle; load generic one
-        require(['discovery.config' + version]);
+        require(['discovery.config.js' + version]);
       });
     };
   } catch (e) {
     load = function () {
       // on errors, just fallback to normal config
-      require(['discovery.config' + version]);
+      require(['discovery.config.js' + version]);
     };
   }
 
   (function checkLoad() {
-    if (window.requirejs) {
+    if (window.requirejs && typeof load === 'function') {
       return load();
     }
     setTimeout(checkLoad, 10);
