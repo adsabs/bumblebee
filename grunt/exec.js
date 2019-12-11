0c4b7a62254c34d7d57ed2fa1595fb2f6f547a93
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2019-12-11 12:47:15 -0500

    fix test

--- grunt/exec.js
+++ grunt/exec.js
@@ -7,31 +7,35 @@
 module.exports = {
   // this is necessary to make the library AMD compatible
   convert_dsjslib: {
-    cmd: 'node node_modules/requirejs/bin/r.js -convert src/libs/dsjslib src/libs/dsjslib'
+    cmd:
+      'node node_modules/requirejs/bin/r.js -convert src/libs/dsjslib src/libs/dsjslib',
   },
   convert_requirejs: {
-    cmd: 'node_modules/.bin/uglifyjs src/libs/requirejs/require.js -c -m -o src/libs/requirejs/require.js'
+    cmd:
+      'node_modules/.bin/uglifyjs src/libs/requirejs/require.js -c -m -o src/libs/requirejs/require.js',
   },
   latest_commit: {
-    cmd: 'git rev-parse --short=7 --verify HEAD | cat > git-latest-commit'
+    cmd: 'git rev-parse --short=7 --verify HEAD | cat > git-latest-commit',
   },
   latest_tag: {
-    cmd: 'git describe --abbrev=0 | cat > git-latest-release-tag'
+    cmd: 'git describe --abbrev=0 | cat > git-latest-release-tag',
   },
   git_current_tag: {
-    cmd: 'git describe --tags --abbrev=0 > .tag'
+    cmd: 'git describe --tags --abbrev=0 > .tag',
   },
   npm_install: {
-    cmd: 'npm install --no-package-lock --no-shrinkwrap'
+    cmd: 'npm install --no-package-lock --no-shrinkwrap',
   },
-  convert_enzyme: {
-    cmd: "mkdir src/libs/enzyme && ./node_modules/.bin/browserify --standalone enzyme -x 'react/addons' -x 'react/lib/ReactContext' -x 'react/lib/ExecutionEnvironment' node_modules/enzyme/build/index.js > src/libs/enzyme/enzyme.js"
+  install_enzyme: {
+    cmd:
+      'mkdir src/libs/enzyme && wget -q -O src/libs/enzyme/index.js https://gitcdn.link/repo/thostetler/enzyme/7c39296c83ed3a8fc09907c9d013aee8d91bde5e/enzyme.2.7.1.js',
   },
   'nyc-instrument': {
-    cmd: 'node_modules/.bin/nyc instrument dist/js/ test/coverage/instrument'
+    cmd: 'node_modules/.bin/nyc instrument dist/js/ test/coverage/instrument',
   },
   'coveralls-report': {
-    cmd: 'cat test/coverage/reports/lcov/lcov.info | ./node_modules/coveralls/bin/coveralls.js'
+    cmd:
+      'cat test/coverage/reports/lcov/lcov.info | ./node_modules/coveralls/bin/coveralls.js',
   },
-  'server': 'node server'
+  server: 'node server',
 };
