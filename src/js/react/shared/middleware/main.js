676cb403b514299daf9d6e7f3bf07d78a3278499
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2020-04-27 15:33:31 -0400

    Expose dispatch and getState

--- src/js/react/shared/middleware/main.js
+++ src/js/react/shared/middleware/main.js
@@ -1,5 +1,5 @@
-define([], function() {
-  const getInitialData = ({ trigger }, { dispatch }) => (next) => (action) => {
+define(['../helpers'], function({ middleware }) {
+  const getInitialData = middleware(({ trigger, next, action, dispatch }) => {
     next(action);
 
     if (action.type === 'GET_INITIAL_DATA') {
@@ -7,9 +7,18 @@ define([], function() {
         dispatch({ type: 'SET_INITIAL_DATA', result });
       });
     }
-  };
+  });
+
+  const injector = middleware(
+    ({ trigger, next, action, dispatch, getState }) => {
+      next(action);
+
+      trigger('_injector', { dispatch, getState });
+    }
+  );
 
   return {
     getInitialData,
+    injector,
   };
 });
