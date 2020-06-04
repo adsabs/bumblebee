6ceca3be35b9696714d96f23710b4cd792ccf22b
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2020-06-04 16:00:34 -0400

    Allow removal of author names on edit
    Pressing delete on an author name now removes that author, not the first
    entry

--- src/js/react/MyAdsDashboard/components/CitationsEntry.jsx.js
+++ src/js/react/MyAdsDashboard/components/CitationsEntry.jsx.js
@@ -26,6 +26,15 @@ define(['underscore', 'react', 'react-bootstrap', 'react-prop-types'], function(
         ...initialState,
         ...props.initialState,
       };
+
+      if (props.initialState.entries && props.initialState.entries.length > 0) {
+        // initialize array of entries with some ids
+        this.state.entries = props.initialState.entries.map((e) => ({
+          ...e,
+          id: uniqueId(),
+        }));
+      }
+
       this.inputRef = null;
     }
 
@@ -105,7 +114,10 @@ define(['underscore', 'react', 'react-bootstrap', 'react-prop-types'], function(
       let valid = true;
       let error = '';
 
-      if (type === 'ORCiD' && !text.match(/^\d{4}-?\d{4}-?\d{4}-?\d{3}[X\d]$/)) {
+      if (
+        type === 'ORCiD' &&
+        !text.match(/^\d{4}-?\d{4}-?\d{4}-?\d{3}[X\d]$/)
+      ) {
         // orcid formatting is off
         valid = false;
         error = 'ORCiD must in the format: 9999-9999-9999-9999';
