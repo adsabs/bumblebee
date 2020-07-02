3b884a60354c055d0adfb7904ae65c895d49f04c
Roman Chyla <roman.chyla@gmail.com>
2020-07-02 16:10:12 -0400

    Adding forgotten updates

--- src/js/components/experiments.js
+++ src/js/components/experiments.js
@@ -1,62 +1,83 @@
-define(['underscore', 
-        'jquery',
-        'js/components/generic_module',
-        'js/mixins/dependon',
-        'analytics'], 
-        function(_, 
-                 $,
-                 GenericModule,
-                 Dependon,
-                 analytics
-                 ) {
-    
-    var Experiments = GenericModule.extend({
-        initialize: function() {
-          // store all metadata entries here
-          this.isRunning = false;
-        },
-    
-        activate: function(beehive, app) {
-            this.setApp(app);
-            this.setBeeHive(beehive);
-            var pubsub = this.getPubSub();
-            pubsub.subscribe(
-                pubsub.ARIA_ANNOUNCEMENT,
-                _.bind(this.onAppStarted, this)
+define([
+  'underscore',
+  'jquery',
+  'js/components/generic_module',
+  'js/mixins/dependon',
+  'analytics',
+  'js/components/pubsub_events',
+], function(_, $, GenericModule, Dependon, analytics, PubsubEvents) {
+  var Experiments = GenericModule.extend({
+    initialize: function() {
+      // store all metadata entries here
+      this.isRunning = false;
+    },
+
+    activate: function(beehive, app) {
+      this.setApp(app);
+      this.setBeeHive(beehive);
+      var pubsub = this.getPubSub();
+
+      if (!window.gtag) {
+        window.gtag = function() {
+          if (_.isArray(window.dataLayer)) {
+            window.dataLayer.push(arguments);
+          }
+        };
+        window.gtag('event', 'optimize.callback', {
+          callback: (value, name) => {
+            console.log(
+              'Experiment with ID: ' + name + ' is on variant: ' + value
             );
           },
-    
-        onAppStarted: function() {
+        });
+      }
 
-            if (!window.gtag) {
-                window.gtag = function () {dataLayer && dataLayer.push(arguments)}
-                gtag('event', 'optimize.callback', {
-                    callback: (value, name) => console.log(
-                        'Experiment with ID: ' + name + ' is on variant: ' + value)
-                });
+      pubsub.subscribe(
+        pubsub.APP_BOOTSTRAPPED,
+        _.bind(this.onAppStarted, this)
+      );
+    },
 
-            }
-            
-            this.toggleOptimize();
-            
-        },
+    /**
+     *
+     * callback that can be used by external components; they can listen to BBB and then run their experiment
+     *
+     * */
+    subscribe: function(event, callback) {
+      var pubsub = this.getPubSub();
+      if (PubsubEvents[event]) {
+        pubsub.subscribe(PubsubEvents[event], callback);
+      }
+    },
 
-        toggleOptimize: function() {
-            if (!dataLayer) {
-                console.warn('Optimize is not available, we are not running any experiment');
-                return;
-            }
+    subscribeOnce: function(event, callback) {
+      var pubsub = this.getPubSub();
+      if (PubsubEvents[event]) {
+        pubsub.subscribeOnce(PubsubEvents[event], callback);
+      }
+    },
 
-            if (this.isRunning) {
-                dataLayer.push({'event': 'optimize.deactivate'});
-            }
-            else {
-                dataLayer.push({'event': 'optimize.activate'});
-            }
-            this.isRunning = !this.isRunning;
-        }
-    });
-    _.extend(Experiments.prototype, Dependon.BeeHive, Dependon.App);
+    onAppStarted: function() {
+      this.toggleOptimize();
+    },
 
-    return Experiments;
-});
+    toggleOptimize: function() {
+      if (!window.dataLayer) {
+        console.warn(
+          'Optimize is not available, we are not running any experiment'
+        );
+        return;
+      }
+
+      if (this.isRunning) {
+        window.dataLayer.push({ event: 'optimize.deactivate' });
+      } else {
+        window.dataLayer.push({ event: 'optimize.activate' });
+      }
+      this.isRunning = !this.isRunning;
+    },
+  });
+  _.extend(Experiments.prototype, Dependon.BeeHive, Dependon.App);
+
+  return Experiments;
+})
