91731d20c21ab0e88b568e722b23a3945e6e7409
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2021-12-03 02:57:10 -0500

    Fix author list semi-colon issue
    fixes user reported bug, which caused the author list to remove trailing
    semi-colon from initial list (before expanding).  Which ran two author
    names together.

--- src/js/widgets/abstract/widget.js
+++ src/js/widgets/abstract/widget.js
@@ -218,7 +218,7 @@ define([
 
     toggleMoreAuthors: function() {
       this.$('.author.extra').toggleClass('hide');
-      this.$('.author.extra-dots').toggleClass('hide');
+      this.$('.author .extra-dots').toggleClass('hide');
       if (this.$('.author.extra').hasClass('hide')) {
         this.$('#toggle-more-authors').text('Show all authors');
       } else {
