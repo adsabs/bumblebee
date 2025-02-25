da2bad04da67b9a885853cba7a5eafe599d99777
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2025-02-25 14:50:34 -0500

    BUMBLEBEE-Y

--- src/js/dark-mode-switch.js
+++ src/js/dark-mode-switch.js
@@ -1,28 +1,42 @@
 define(['analytics'], function(analytics) {
+  const STORAGE_KEY = 'darkSwitch';
   let darkSwitch;
+  const getDarkSwitchValue = () => {
+    try {
+      localStorage.getItem('darkSwitch');
+    } catch (e) {
+      return null;
+    }
+  };
+
+  const setDarkSwitchValue = (val) => {
+    try {
+      localStorage.setItem('darkSwitch', val);
+    } catch (e) {
+      // localStorage is disabled
+    }
+  };
 
   const turnOnDarkMode = (save) => {
     document.body.setAttribute('data-theme', 'dark');
     darkSwitch.classList.add('darkModeOn');
     darkSwitch.setAttribute('title', 'Turn off dark mode');
-    if (save) localStorage.setItem('darkSwitch', 'on');
+    if (save) {
+      setDarkSwitchValue('on');
+    }
   };
 
   const turnOffDarkMode = (save) => {
     document.body.removeAttribute('data-theme');
     darkSwitch.classList.remove('darkModeOn');
     darkSwitch.setAttribute('title', 'Turn on dark mode');
-    if (save) localStorage.setItem('darkSwitch', 'off');
+    if (save) {
+      setDarkSwitchValue('off');
+    }
   };
 
   const emitAnalytics = (action, label) => {
-    analytics(
-      'send',
-      'event',
-      'uitheme', // category
-      action,
-      label
-    );
+    analytics('send', 'event', 'uitheme', action, label);
   };
 
   const toggle = () => {
@@ -37,15 +51,14 @@ define(['analytics'], function(analytics) {
 
   const init = () => {
     darkSwitch = document.getElementById('darkSwitch');
+    if (!darkSwitch) return;
     darkSwitch.classList.remove('hidden');
 
+    const savedMode = getDarkSwitchValue();
     // 1. check app setting
-    if (localStorage.getItem('darkSwitch') !== null) {
-      if (localStorage.getItem('darkSwitch') === 'on') {
-        turnOnDarkMode(false);
-      } else {
-        turnOffDarkMode(false);
-      }
+    if (savedMode !== null) {
+      // eslint-disable-next-line no-unused-expressions
+      savedMode !== 'on' ? turnOffDarkMode(false) : turnOnDarkMode(false);
     } else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
       // 2. check system setting
       turnOnDarkMode(false);
