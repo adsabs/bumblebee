4412a29e5d4d067e74ad3b57a01c0563d1e83208
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2025-04-07 14:44:28 -0400

    SCIX-555 handle null format value in export control

--- src/js/widgets/author_affiliation_tool/components/ExportFormatControl.jsx.js
+++ src/js/widgets/author_affiliation_tool/components/ExportFormatControl.jsx.js
@@ -2,21 +2,26 @@ define(['react'], function(React) {
   /**
    * Dropdown containing export format strings for the user to select
    */
-  const ExportFormatControl = ({ formats, format, onChange }) => (
-    <select
-      onChange={(e) => onChange(e.target.value)}
-      value={format}
-      id="export-format-control"
-      className="form-control"
-      title="Select a format"
-    >
-      {formats.map((f) => (
-        <option key={f} value={f}>
-          {f}
-        </option>
-      ))}
-    </select>
-  );
+  const ExportFormatControl = (props) => {
+    if (!props.formats || props.formats.length === 0) {
+      return null;
+    }
+    return (
+      <select
+        onChange={(e) => props.onChange(e.target.value)}
+        value={props.format}
+        id="export-format-control"
+        className="form-control"
+        title="Select a format"
+      >
+        {props.formats.map((f) => (
+          <option key={f} value={f}>
+            {f}
+          </option>
+        ))}
+      </select>
+    );
+  };
 
   return ExportFormatControl;
 });
