64ab4249af724cec47daf2ac99640096b155f7f8
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2025-04-14 10:19:50 -0400

    Update toc_controller.js

--- src/js/page_managers/toc_controller.js
+++ src/js/page_managers/toc_controller.js
@@ -166,7 +166,6 @@ define([
       if (subView) {
         widgetName = widgetName + '__' + subView;
       }
-      this.widgets.tocWidget.collection.selectOne(widgetName);
 
       // Add defensive check to ensure tocWidget and its collection exist
       if (this.widgets.tocWidget && this.widgets.tocWidget.collection) {
